73526feffb9cd25d2499531ec140f1385ff208aa
mozi <mozim@outlook.com>
2025-03-09 10:46:03 -0700

    new file:   tools/database.sql

---
+++ tools/database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE eternal;
+USE eternal;
+
+CREATE TABLE `users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `duration` int NOT NULL,
+  `cooldown` int NOT NULL,
+  `endtime` date NOT NULL,
+  `clients` int NOT NULL,
+  `api_key` varchar(255) DEFAULT NULL
+);
+
+INSERT INTO users VALUES ('admin', 'admin', 0, 1, 0, 0, "2040-06-18", -1, '');
